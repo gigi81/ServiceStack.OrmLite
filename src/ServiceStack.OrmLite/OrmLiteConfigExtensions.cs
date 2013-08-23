a6fcc8d6be0aaa0cc092a3c8045f0964253c3588
Luigi Grilli <luigi.grilli@gmail.com>
2013-08-23 11:45:14 +0100

    Put some order into OrmLiteConfigExtensions  class
    Some refactoring on OrmLiteConfigExtensions class. Now using
    ConcurrentDictionary for models cache

--- src/ServiceStack.OrmLite/OrmLiteConfigExtensions.cs
+++ src/ServiceStack.OrmLite/OrmLiteConfigExtensions.cs
@@ -14,91 +14,75 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Reflection;
-using System.Threading;
-using ServiceStack.Common;
 using ServiceStack.DataAnnotations;
+using ServiceStack.Net30.Collections.Concurrent;
 using ServiceStack.Text;
 
 namespace ServiceStack.OrmLite
 {
     internal static class OrmLiteConfigExtensions
     {
-        private static Dictionary<Type, ModelDefinition> typeModelDefinitionMap = new Dictionary<Type, ModelDefinition>();
+        private static readonly ConcurrentDictionary<Type, ModelDefinition> _cache = new ConcurrentDictionary<Type, ModelDefinition>();
 
         private static bool IsNullableType(Type theType)
         {
-            return (theType.IsGenericType
-                && theType.GetGenericTypeDefinition() == typeof(Nullable<>));
-        }
-
-        internal static bool CheckForIdField(IEnumerable<PropertyInfo> objProperties)
-        {
-            // Not using Linq.Where() and manually iterating through objProperties just to avoid dependencies on System.Xml??
-            foreach (var objProperty in objProperties)
-            {
-                if (objProperty.Name != OrmLiteConfig.IdField) continue;
-                return true;
-            }
-            return false;
+            return (theType.IsGenericType && theType.GetGenericTypeDefinition() == typeof(Nullable<>));
         }
 
         internal static void ClearCache()
         {
-            typeModelDefinitionMap = new Dictionary<Type, ModelDefinition>();
+            _cache.Clear();
         }
 
-        public static ModelDefinition Init(this Type modelType)
+        internal static ModelDefinition Init(this Type modelType)
         {
             return modelType.GetModelDefinition();
         }
 
         internal static ModelDefinition GetModelDefinition(this Type modelType)
         {
-            ModelDefinition modelDef;
+            return _cache.GetOrAdd(modelType, CreateModelDefinition);
+        }
 
-            if (typeModelDefinitionMap.TryGetValue(modelType, out modelDef))
-                return modelDef;
-
+        private static ModelDefinition CreateModelDefinition(Type modelType)
+        {
             var modelAliasAttr = modelType.FirstAttribute<AliasAttribute>();
             var schemaAttr = modelType.FirstAttribute<SchemaAttribute>();
-            modelDef = new ModelDefinition {
+            var modelDef = new ModelDefinition
+            {
                 ModelType = modelType,
                 Name = modelType.Name,
                 Alias = modelAliasAttr != null ? modelAliasAttr.Name : null,
                 Schema = schemaAttr != null ? schemaAttr.Name : null
             };
 
-            modelDef.CompositeIndexes.AddRange(
-                modelType.GetCustomAttributes(typeof(CompositeIndexAttribute), true).ToList()
-                .ConvertAll(x => (CompositeIndexAttribute)x));
+            modelDef.CompositeIndexes.AddRange(modelType.GetCustomAttributes(typeof(CompositeIndexAttribute), true).OfType<CompositeIndexAttribute>());
 
-            var objProperties = modelType.GetProperties(
-                BindingFlags.Public | BindingFlags.Instance).ToList();
+            var objProperties = modelType.GetProperties(BindingFlags.Public | BindingFlags.Instance);
 
-            var hasIdField = CheckForIdField(objProperties);
+            var hasIdField = objProperties.Any(p => p.Name == OrmLiteConfig.IdField);
 
-            var i = 0;
-            foreach (var propertyInfo in objProperties)
+            for (var i = 0; i < objProperties.Length; i++)
             {
+                var propertyInfo = objProperties[i];
                 var sequenceAttr = propertyInfo.FirstAttribute<SequenceAttribute>();
-                var computeAttr= propertyInfo.FirstAttribute<ComputeAttribute>();
+                var computeAttr = propertyInfo.FirstAttribute<ComputeAttribute>();
                 var pkAttribute = propertyInfo.FirstAttribute<PrimaryKeyAttribute>();
                 var decimalAttribute = propertyInfo.FirstAttribute<DecimalLengthAttribute>();
                 var belongToAttribute = propertyInfo.FirstAttribute<BelongToAttribute>();
                 var isFirst = i++ == 0;
 
-                var isPrimaryKey = propertyInfo.Name == OrmLiteConfig.IdField || (!hasIdField && isFirst)
-                    || pkAttribute != null;
+                var isPrimaryKey = propertyInfo.Name == OrmLiteConfig.IdField || (!hasIdField && isFirst) || pkAttribute != null;
 
                 var isNullableType = IsNullableType(propertyInfo.PropertyType);
 
                 var isNullable = (!propertyInfo.PropertyType.IsValueType
-                                   && propertyInfo.FirstAttribute<RequiredAttribute>() == null)
+                                  && propertyInfo.FirstAttribute<RequiredAttribute>() == null)
                                  || isNullableType;
 
                 var propertyType = isNullableType
-                    ? Nullable.GetUnderlyingType(propertyInfo.PropertyType)
-                    : propertyInfo.PropertyType;
+                                       ? Nullable.GetUnderlyingType(propertyInfo.PropertyType)
+                                       : propertyInfo.PropertyType;
 
                 var aliasAttr = propertyInfo.FirstAttribute<AliasAttribute>();
 
@@ -116,64 +100,52 @@ namespace ServiceStack.OrmLite
                 if (decimalAttribute != null && stringLengthAttr == null)
                     stringLengthAttr = new StringLengthAttribute(decimalAttribute.Precision);
 
-                var fieldDefinition = new FieldDefinition {
+                var fieldDefinition = new FieldDefinition
+                {
                     Name = propertyInfo.Name,
                     Alias = aliasAttr != null ? aliasAttr.Name : null,
                     FieldType = propertyType,
                     PropertyInfo = propertyInfo,
                     IsNullable = isNullable,
                     IsPrimaryKey = isPrimaryKey,
-                    AutoIncrement =
-                        isPrimaryKey &&
-                        propertyInfo.FirstAttribute<AutoIncrementAttribute>() != null,
+                    AutoIncrement = isPrimaryKey && propertyInfo.FirstAttribute<AutoIncrementAttribute>() != null,
                     IsIndexed = isIndex,
                     IsUnique = isUnique,
-                    FieldLength =
-                        stringLengthAttr != null
-                            ? stringLengthAttr.MaximumLength
-                            : (int?)null,
-                    DefaultValue =
-                        defaultValueAttr != null ? defaultValueAttr.DefaultValue : null,
-                    ForeignKey =
-                        foreignKeyAttr == null
-                            ? referencesAttr == null
-                                  ? null
-                                  : new ForeignKeyConstraint(referencesAttr.Type)
-                            : new ForeignKeyConstraint(foreignKeyAttr.Type,
-                                                       foreignKeyAttr.OnDelete,
-                                                       foreignKeyAttr.OnUpdate,
-                                                       foreignKeyAttr.ForeignKeyName),
+                    FieldLength = stringLengthAttr != null ? stringLengthAttr.MaximumLength : (int?)null,
+                    DefaultValue = defaultValueAttr != null ? defaultValueAttr.DefaultValue : null,
+                    ForeignKey = GetForeignKeyConstraint(foreignKeyAttr, referencesAttr),
                     GetValueFn = propertyInfo.GetPropertyGetterFn(),
                     SetValueFn = propertyInfo.GetPropertySetterFn(),
                     Sequence = sequenceAttr != null ? sequenceAttr.Name : string.Empty,
                     IsComputed = computeAttr != null,
-                    ComputeExpression =
-                        computeAttr != null ? computeAttr.Expression : string.Empty,
+                    ComputeExpression = computeAttr != null ? computeAttr.Expression : string.Empty,
                     Scale = decimalAttribute != null ? decimalAttribute.Scale : (int?)null,
-                    BelongToModelName = belongToAttribute != null ? belongToAttribute.BelongToTableType.GetModelDefinition().ModelName : null, 
+                    BelongToModelName = belongToAttribute != null ? belongToAttribute.BelongToTableType.GetModelDefinition().ModelName : null,
                 };
 
                 if (propertyInfo.FirstAttribute<IgnoreAttribute>() != null)
-                  modelDef.IgnoredFieldDefinitions.Add(fieldDefinition);
+                    modelDef.IgnoredFieldDefinitions.Add(fieldDefinition);
                 else
-                  modelDef.FieldDefinitions.Add(fieldDefinition);                
+                    modelDef.FieldDefinitions.Add(fieldDefinition);
             }
 
             modelDef.SqlSelectAllFromTable = "SELECT {0} FROM {1} ".Fmt(OrmLiteConfig.DialectProvider.GetColumnNames(modelDef),
-                                                                        OrmLiteConfig.DialectProvider.GetQuotedTableName(
-                                                                            modelDef));
-            Dictionary<Type, ModelDefinition> snapshot, newCache;
-            do
-            {
-                snapshot = typeModelDefinitionMap;
-                newCache = new Dictionary<Type, ModelDefinition>(typeModelDefinitionMap);
-                newCache[modelType] = modelDef;
-
-            } while (!ReferenceEquals(
-                Interlocked.CompareExchange(ref typeModelDefinitionMap, newCache, snapshot), snapshot));
-
+                                                                        OrmLiteConfig.DialectProvider.GetQuotedTableName(modelDef));
             return modelDef;
         }
 
+        private static ForeignKeyConstraint GetForeignKeyConstraint(ForeignKeyAttribute foreignKeyAttr, ReferencesAttribute referencesAttr)
+        {
+            if (foreignKeyAttr != null)
+                return new ForeignKeyConstraint(foreignKeyAttr.Type,
+                                                foreignKeyAttr.OnDelete,
+                                                foreignKeyAttr.OnUpdate,
+                                                foreignKeyAttr.ForeignKeyName);
+
+            if (referencesAttr != null)
+                return new ForeignKeyConstraint(referencesAttr.Type);
+
+            return null;
+        }
     }
 }
